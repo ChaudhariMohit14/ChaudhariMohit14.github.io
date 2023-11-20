1cb993c6374261537b7599468b10f1c54575bd58
unknown <chaudharimohit1410@gmail.com>
2023-11-20 14:27:28 +0530

    Submit

--- scripts/script.js
+++ scripts/script.js
@@ -79,3 +79,37 @@ document.querySelector(".contact").addEventListener("click", ()=>{
         m.classList.add("m-t");
     }
 });
+
+
+//  Send Email from contact form
+
+function sendMail(){
+
+ var parms={
+    name : document.getElementById("name1").value,
+    email : document.getElementById("email1").value,
+    project : document.getElementById("project1").value,
+    message : document.getElementById("message1").value,
+ };
+console.log("hello")
+ const serviceid="service_bfa2vd6";
+ const templateid="template_7axkaig";
+
+ emailjs
+ .send(serviceid,templateid,parms)
+ .then((res) => {
+    document.getElementById("name1").value="";
+    document.getElementById("email1").value="";
+    document.getElementById("project1").value="";
+    document.getElementById("message1").value="";
+
+    console.log(res);
+
+    alert("Your Message Sent Successfully");
+ })
+ .catch((err) => {
+    console.log(err);
+    alert("Error occured please try again later");
+ } )
+
+}
